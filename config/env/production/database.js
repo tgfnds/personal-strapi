5856eab0ec86cd4dc571b8b9cb91d846da7593cd
Tiago Fernandes <tgfnds23@gmail.com>
2022-01-26 15:38:38 +0000

    changed db settings to deploy on digital ocean app platform

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,23 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({env}) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      }
+module.exports = () => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      options: {},
     },
-    debug: false
-  }
+  },
 });
